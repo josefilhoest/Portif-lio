fbd1d95a140cc2d94fb59c3f7cce0ff2856e23df
josefilhoest <josefilhoelineide2236@hotmail.com>
2026-01-16 19:44:27 -0300

    Adiciona arquivos de imagem e cria estrutura inicial do portfólio em HTML, CSS e JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Exemplo simples de Scroll Suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
